d537c593e05a84842619b96b1c9ebb4699960a6f
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-16 19:00:31 +0200

    Update App.tsx

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  const [state, setState] = useState({ isAdmin: false, userName: "Olga" });
+  const { isAdmin } = state;
+  const assignAdmin = () => {
+    setState({ ...state, isAdmin: true });
+  };
+  return (
+    <>
+      <h1>Hello user, you is {isAdmin ? "SUPER ADMIN" : "a user"}</h1>
+      <button onClick={assignAdmin}>Assign admin</button>
+    </>
+  );
 }
 
 export default App;
